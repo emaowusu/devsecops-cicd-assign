b6e32080fa6e31ef402590b05642f25511a2784d
eowusu <owusuaddaiemmanuel35@gmail.com>
2026-04-28 05:04:38 +0000

    iP changed to '0.0.0.0'

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
 
